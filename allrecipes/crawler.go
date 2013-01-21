a22d136fe131ec2ac9884951ecbb7cbada88ce71
Jason Cox <JasonWilburCox@gmail.com>
2013-01-21 12:40:09 -0500

    Reading from allrecipes.com

--- allrecipes/crawler.go
+++ allrecipes/crawler.go
@@ -1,14 +1,34 @@
 package allrecipes
 
-const HOSTNAME = "allrecipes.com"
+import (
+  "log"
+  "net/http"
+  "io/ioutil"
+)
+
+const HOSTNAME = "http://allrecipes.com"
 
 func NewReader() <-chan string {
   r := make(chan string)
-  go func() {
-    for {
-      r <- "Hello World"
-    }
-  }()
+
+  go readAllRecipes(r)
 
   return r
+}
+
+func readAllRecipes(r chan<- string) {
+  resp, err := http.Get(HOSTNAME)
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Println("Failed to read from " + HOSTNAME)
+    return
+  }
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return
+  }
+
+  r <- string(body)
 }
