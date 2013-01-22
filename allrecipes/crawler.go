069d4cef1bae80062076e5d89b9f0a2543be2f72
Jason Cox <JasonWilburCox@gmail.com>
2013-01-22 06:43:43 -0500

    Refactor to make more generic. Also point to an actual recipe page

--- allrecipes/crawler.go
+++ allrecipes/crawler.go
@@ -6,22 +6,33 @@ import (
   "io/ioutil"
 )
 
-const HOSTNAME = "http://allrecipes.com"
+const HOSTNAME = "http://allrecipes.com/recipes/"
+
+const RECIPE_VIEW_ALL = "ViewAll.aspx"
+
+var recipeUrlList = []string{"pasta/"}
 
 func NewReader() <-chan string {
   r := make(chan string)
 
-  go readAllRecipes(r)
+  // Fan-in pattern
+  for url := range recipeUrlList {
+    go readPage(translateUrl(recipeUrlList[url]), r)
+  }
 
   return r
 }
 
-func readAllRecipes(r chan<- string) {
-  resp, err := http.Get(HOSTNAME)
+func translateUrl(url string) string {
+  return HOSTNAME + url + RECIPE_VIEW_ALL
+}
+
+func readPage(url string, r chan<- string) {
+  resp, err := http.Get(url)
   defer resp.Body.Close()
 
   if err != nil {
-    log.Println("Failed to read from " + HOSTNAME)
+    log.Println("Failed to read from " + url)
     return
   }
 
@@ -31,4 +42,8 @@ func readAllRecipes(r chan<- string) {
   }
 
   r <- string(body)
+}
+
+func filterRecipeLinks(body string) []string {
+  return []string{body}
 }
