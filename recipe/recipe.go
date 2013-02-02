626cc13e45f67988ddb13c6ee0e9ee901d791a4d
Jason Cox <JasonWilburCox@gmail.com>
2013-02-01 22:52:06 -0500

    Display directions better

--- recipe/recipe.go
+++ recipe/recipe.go
@@ -26,12 +26,22 @@ func ingredientsListWithAmounts(ingredients []string, amounts []string) string {
   return retVal
 }
 
+func directions(directions []string) string {
+  retVal := "Directions:\n"
+
+  for i := range directions {
+    retVal += fmt.Sprintf("\t\t%d) %s\n", i+1, directions[i])
+  }
+
+  return retVal
+}
+
 func (r *Recipe) String() string {
   return fmt.Sprintf("Recipe: %s\n" + "\tLink: %s\n" + "\tImageLink: %s\n" +
                      "\tRating: %s\n" + "\tReadyTime: %sh %sm\n" +
-                     "\tCookTime: %sh %sm\n" + "\t%s\n" + "\tDirections: %s\n",
+                     "\tCookTime: %sh %sm\n" + "\t%s\n" + "\t%s\n",
                      r.Name, r.Link, r.ImageLink, r.Rating, r.ReadyTimeHours,
                      r.ReadyTimeMins, r.CookTimeHours, r.CookTimeMins,
                      ingredientsListWithAmounts(r.Ingredients, r.Amounts),
-                     r.Directions)
+                     directions(r.Directions))
 }
