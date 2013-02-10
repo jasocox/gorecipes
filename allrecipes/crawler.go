5b99c19e04836aa0a3af63d8d1a6cbf5d7e98d8b
Jason Cox <JasonWilburCox@gmail.com>
2013-02-10 18:29:28 -0500

    Refactoring http reading code to it's own file

--- allrecipes/crawler.go
+++ allrecipes/crawler.go
@@ -2,8 +2,6 @@ package allrecipes
 
 import (
   "log"
-  "net/http"
-  "io/ioutil"
   "gorecipes/recipe"
 )
 
@@ -105,22 +103,4 @@ func translateRecipeFromBody(body string, url string) (r recipe.Recipe) {
   r.Directions = translate("Directions", body).([]string)
 
   return
-}
-
-func readBodyFromUrl(url string) (string, error) {
-  resp, err := http.Get(url)
-  defer resp.Body.Close()
-
-  if err != nil {
-    log.Println("Failed to process " + url)
-    return "", err
-  }
-
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    log.Println("Failed to read the body for " + url)
-    return "", err
-  }
-
-  return string(body), nil
 }
---
+++ allrecipes/httpReader.go
@@ -0,0 +1,25 @@
+package allrecipes
+
+import (
+  "log"
+  "net/http"
+  "io/ioutil"
+)
+
+func readBodyFromUrl(url string) (string, error) {
+  resp, err := http.Get(url)
+  defer resp.Body.Close()
+
+  if err != nil {
+    log.Println("Failed to process " + url)
+    return "", err
+  }
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Println("Failed to read the body for " + url)
+    return "", err
+  }
+
+  return string(body), nil
+}
